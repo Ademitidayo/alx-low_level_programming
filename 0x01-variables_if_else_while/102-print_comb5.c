e7f32ec564be395a4b318bc913a41c90f703d47d
Ademitidayo <adenijibimmy08@gmail.com>
2023-07-14 15:46:28 -0700

    corrections 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,22 +12,18 @@ int main(void)
 	{
 		for (j = i; j < 100; j++)
 		{
-			int tens1 = i / 10;
-			int ones1 = i % 10;
-			int tens2 = j / 10;
-			int ones2 = j % 10;
-
-			putchar('0' + tens1);
-			putchar('0' + ones1);
-			putchar(' ');
-
-			putchar('0' + tens2);
-			putchar('0' + ones2);
-
-			if (i != 98 || j != 99)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
