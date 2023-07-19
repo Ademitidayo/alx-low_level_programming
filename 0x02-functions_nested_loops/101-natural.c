1932dad1bbe51973f407ef9e2f21072dfa18c6cb
Ademitidayo <adenijibimmy08@gmail.com>
2023-07-18 21:32:02 -0700

    corrections

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * File: 101-natural.c
+ * Auth: vincent kip
+ */
+
+#include <stdio.h>
+
+/**
+ * main - list all the naturak numbers below 1024 (excluded)
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0
+
+		for (i = 0; i < 1024; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
+				sum += i;
+		}
+
+	printf("%d\n", sum);
+	return (0);
+}
