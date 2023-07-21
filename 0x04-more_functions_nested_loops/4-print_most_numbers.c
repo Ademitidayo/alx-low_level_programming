ffa1610437f3bece3db289d01dd0b4641755f17e
Ademitidayo <adenijibimmy08@gmail.com>
2023-07-20 18:59:43 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 but 2 and 4
+ * Return: numbers from 0-9
+ */
+void print_most_numbers(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+	if (n == 2 || n == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(n + '0');
+	}
+	}
+	_putchar('\n');
+}
