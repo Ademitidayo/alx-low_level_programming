5a549eff670f4ee52567a7440ce07f9fe115a293
Ademitidayo <adenijibimmy08@gmail.com>
2023-08-03 01:25:05 -0700

    corrections

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,46 @@
 #include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
 /**
  * is_palindrome - function that returns 1 if a string is a palindrome and 0
  * @s: input string
- * Return: lenth
+ * Return: 1 if yes, 0 if not
  */
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	if (*s == 0)
 		return (0);
-	else
-		retun(1 + (1 + _strken_recursoin));
-
-	return (0);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
- *_palindrome - cd unction that returns the natural square
- XXZ
- aked tiless[counter] = rev;
+ * _strlen_recursion - checks the characters for palindrome
+ * @s: input string
+ * @i: iterator
+ * @len: length
+ *
+ * Return: 1 if yes, o if not
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * check_pal - returns the lenght of a string
+ * @s: string
+ * @i: iterator
+ * @len: length
+ *
+ * Return: 1 if yes, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,11 @@
 #include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root
- * @i
+ * @i: iterate number
  *
  * : iterate number
  *
@@ -10,10 +13,23 @@
  */
 int _sqrt_recursion(int n)
 {
-	int sqrt = i * i;
-
-	if (sqrt > n)
+	if (n < 0)
 		return (-1);
-	if (sqrt == n)
+	return (actual_sqrt_recursion(n, 0));
+}
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number
+ * @i: iterate number
+ *
+ * Return: square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
 		return (i);
-	return (
+	return (actual_sqrt_recursion(n, i + 1));
+}
