939a551304309611c29654e5cd8412f3227369f8
Ademitidayo <adenijibimmy08@gmail.com>
2023-08-02 22:49:10 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: input string
+ * Return: string in reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}	
---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * is_palindrome - function that returns 1 if a string is a palindrome and 0
+ * @s: input string
+ * Return: lenth
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		retun(1 + (1 + _strken_recursoin));
+
+	return (0);
+}
+/**
+ *_palindrome - cd unction that returns the natural square
+ XXZ
+ aked tiless[counter] = rev;
