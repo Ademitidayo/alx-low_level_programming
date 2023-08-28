4abe2495523acd89585d8c00ce78e1c4e3348568
Ademitidayo <adenijibimmy08@gmail.com>
2023-08-28 16:16:53 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
@@ -6,14 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		count++;
-		h = h -> next;
+		num++;
+		h = h->next;
 	}
 
-	return (count);
+	return (num);
 }
-
