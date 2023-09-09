c70b00409ef658302d4a2332a6563ad9341eaac9
Ademitidayo <adenijibimmy08@gmail.com>
2023-09-08 17:07:14 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int a, b, c = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (c = 0; text_content[c];)
+			c++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(a, text_content, c);
 
-	if (fd == -1 || w == -1)
+	if (a == -1 || b == -1)
 		return (-1);
 
-	close(fd);
+	close(a);
 
 	return (1);
 }
