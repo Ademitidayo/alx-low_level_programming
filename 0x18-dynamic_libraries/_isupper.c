6b36003f2ba9525cda92aba28c2aba31dbd97bbe
ademitidayo <adenijibimmy08@gmail.com>
2023-11-13 07:01:14 -0800

    mandatory completed

---
+++ 0x18-dynamic_libraries/_isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character.
+ * @a: alphabet to be checked
+ * Return: 1 if uppercase and 0 if anything else
+ */
+int _isupper(int a)
+{
+	if (a >= 65 && a <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
