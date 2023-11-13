244213ce05003f0937af9221356cd1f44e5766c1
Ademitidayo <adenijibimmy08@gmail>
2023-11-13 11:20:32 +0100

    white space

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,4 @@
-#include"lists.h"
+#include "lists.h"
 /**
  * print_dlistint - printsalltheelementsofadlistint_tlist.
  * @h: list head
@@ -6,15 +6,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+const dlistint_t *current = h;
+size_t count = 0;
 
-    while (current!=NULL)
-    {
-        printf ("%d\n",current->n);
-        current = current -> next;
-        count++;
-    }
-
-    return count;
+while (current!=NULL)
+{
+printf ("%d\n",current->n);
+current = current -> next;
+count++;
+}
+return count;
 }
